b40a3fe8b7bc38c68c0f7073e1824e1df9e53292
Fabricio Carvalho <fcarvalho1999@gmail.com>
2021-09-02 21:52:31 -0300

    Better styling

--- src/views/GetOne.vue
+++ src/views/GetOne.vue
@@ -1,34 +1,37 @@
 <template>
-  <div class="division">
-    <div>
-      <SideMenu />
+  <div v-if="!this.$store.state.quotes.loading">
+    <div class="division">
+      <div>
+        <SideMenu />
+      </div>
+      <v-row justify="center" align="center">
+        <v-col cols="4">
+          <div class="pt-12">
+            <v-card height="250" width="500">
+              <v-card-title>{{ this.quotes.anime }}</v-card-title>
+              <v-card-subtitle>
+                <p class="pr-2 scroll text-justify">
+                  {{ this.quotes.quote }}
+                </p>
+              </v-card-subtitle>
+              <v-card-subtitle class="pt-0">
+                {{ this.quotes.character }}
+              </v-card-subtitle>
+            </v-card>
+          </div>
+        </v-col>
+      </v-row>
     </div>
-    <v-row justify="center" align="center">
-      <v-col cols="4">
-        <div class="pt-12">
-          <v-card height="250" width="500">
-            <v-card-title>{{ this.quotes.anime }}</v-card-title>
-            <v-card-subtitle>
-              <p class="pr-2 scroll text-justify">
-                {{ this.quotes.quote }}
-              </p>
-            </v-card-subtitle>
-            <v-card-subtitle class="pt-0">
-              {{ this.quotes.character }}
-            </v-card-subtitle>
-          </v-card>
-        </div>
-      </v-col>
-    </v-row>
+  </div>
+  <div v-else>
+    <Loading />
   </div>
 </template>
 
 
 <script>
-import SideMenu from "../components/SideMenu.vue";
 export default {
   name: "About",
-  components: { SideMenu },
   data() {
     return {
       quotes: "",
--- src/views/GetTen.vue
+++ src/views/GetTen.vue
@@ -3,37 +3,41 @@
     <div style="width: 30%">
       <SideMenu />
     </div>
-    <div>
-      <v-row class="pt-8" justify="center">
-        <v-btn @click="getTenQuotes()">Update</v-btn>
-      </v-row>
-      <v-row class="pt-4" justify="center">
-        <v-col class="px-8" v-for="(item, id) in quotes" :key="id" cols="4">
-          <v-card height="240" width="500">
-            <v-card-title>{{ item.anime }}</v-card-title>
-            <v-card-subtitle class="pb-0">
-              <p class="pr-2 scroll text-justify">
-                {{ item.quote }}
-              </p>
-            </v-card-subtitle>
-            <v-card-subtitle class="pt-0">
-              {{ item.character }}
-            </v-card-subtitle>
-          </v-card>
-        </v-col>
-      </v-row>
+
+    <div v-if="!this.$store.state.quotes.loading">
+      <div class="page-scroll">
+        <v-row class="pt-8" justify="center">
+          <v-btn @click="getTenQuotes()">Update</v-btn>
+        </v-row>
+        <v-row class="pt-4" justify="center">
+          <v-col class="px-8" v-for="(item, id) in quotes" :key="id" cols="4">
+            <v-card height="240" width="500">
+              <v-card-title class="text-subtitle-1">{{
+                item.anime
+              }}</v-card-title>
+              <v-card-subtitle class="pb-0">
+                <p class="pr-2 card-scroll text-justify">
+                  {{ item.quote }}
+                </p>
+              </v-card-subtitle>
+              <v-card-subtitle class="pt-0">
+                {{ item.character }}
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+    <div v-else>
+      <Loading />
     </div>
   </div>
 </template>
 
 
 <script>
-import SideMenu from "../components/SideMenu.vue";
 export default {
   name: "GetTen",
-  components: {
-    SideMenu,
-  },
   data() {
     return {
       quotes: [],
@@ -59,9 +63,14 @@ export default {
 </script>
 
 <style>
-.scroll {
+.card-scroll {
   height: 120px;
   overflow-y: auto;
+}
+.page-scroll {
+  height: 100vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
 }
 .division {
   display: flex;
