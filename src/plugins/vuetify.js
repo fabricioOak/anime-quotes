b50b0983687f2b1543c8eaf744844e46701923d6
Fabricio Carvalho <fcarvalho1999@gmail.com>
2021-11-05 00:30:20 -0300

    Themes update

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,5 +4,27 @@ import Vuetify from "vuetify/lib/framework";
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  theme: { dark: true },
+  theme: {
+    themes: {
+      light: {
+        primary: "#6e5b9a",
+        secondary: "#b28ead",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107"
+      },
+      dark: {
+        primary: "#4a148c",
+        secondary: "#f50057",
+        accent: "#ff6f00",
+        error: "#f44336",
+        warning: "#ff9800",
+        info: "#2196f3",
+        success: "#4caf50"
+      }
+    }
+  }
 });
+
